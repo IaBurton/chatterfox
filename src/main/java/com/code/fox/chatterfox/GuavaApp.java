90b2ce540fded0fff158579d75956856c60b1a17
Eric Chi <ericjaychi@gmail.com>
2015-02-09 11:12:35 -0800

    Added guava dependencies and a hello world method.

---
+++ src/main/java/com/code/fox/chatterfox/GuavaApp.java
@@ -0,0 +1,11 @@
+import com.google.common.base.Joiner;
+ 
+public class GuavaApp
+{
+    public static void main(String[] args) 
+    {
+        String[] words = {"Hello", "World", "using", "the", "Guava", "library"};
+        String joined = Joiner.on(" ").join(words);
+        System.out.println(joined);
+    }
+}
