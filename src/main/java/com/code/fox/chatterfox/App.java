f08afe6a7341fecd4c1f8a1466f192e6289c5745
Javier Sanchez <javiers1@csupomona.edu>
2015-02-09 23:37:13 -0800

    Moved Eric's Maven import code, ready for Maven package

--- src/main/java/com/code/fox/chatterfox/App.java
+++ src/main/java/com/code/fox/chatterfox/App.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import com.code.fox.chatterfox.data.provider.AnonManager;
+import com.google.common.base.Joiner;
 
 @Configuration
 @EnableAutoConfiguration
@@ -28,6 +29,12 @@ public class App {
 		//Imported by Javier Sanchez
 		LocalTime localTime = new LocalTime();
 		System.out.println("Current Time is: " + localTime.toString());
+		
+		//Google Guava example. Pulled from Maven
+		//Imported by Eric Chi
+		String[] words = {"Hello", "World", "using", "the", "Guava", "library"};
+        String joined = Joiner.on(" ").join(words);
+        System.out.println(joined);
 		
 		SpringApplication.run(App.class, args);
 	}
