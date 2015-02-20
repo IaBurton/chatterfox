c5729d6ca1faeb2f97210ba35eee1f1cdc46e18f
Javier Sanchez <javiers1@csupomona.edu>
2015-02-19 16:55:57 -0800

    Front end login built, still needs functionality

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,11 +1,57 @@
+
+/*$(window).load(function(){     	
+  $('#login1').hide();
+  $('#login').show();
+  $('#register').hide();
+
+})
+*/
+
+$('#toggle-login-anon').click(function(){
+  if($('#login').is(':visible')){
+    $('#login').hide();
+  }
+  else { 
+    $('#login1').hide()
+    $('#register').hide()
+    $('#login').show();
+  }
+});
+
+$('#toggle-login-accn').click(function(){
+  if($('#login1').is(':visible')){
+    $('#login1').hide();
+  }
+  else { 
+    $('#login').hide()
+    $('#register').hide()
+    $('#login1').show();
+  }
+});
+
+$('#toggle-register').click(function(){
+  if($('#register').is(':visible')){
+    $('#register').hide();
+  }
+  else { 
+    $('#login').hide()
+    $('#login1').hide()
+    $('#register').show();
+  }
+});
+
 $(document).ready(function ()
 {
+  $('#login1').hide();
+  $('#login').show();
+  $('#register').hide();
 	$('#user_login').keyup(function(event){
     	if(event.keyCode == 13){
         	$('#enter').click();
     	}
 	});
 });
+
 
 function loginUser() {
 
